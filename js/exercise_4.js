12feb6e87c0017101d99207eaa6d39615e859173
gooberfer <gooberfer@gmail.com>
2016-05-07 20:41:08 -0500

    Update exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -69,3 +69,22 @@ map.on('click',function(e){
     $('#info').fadeOut(200);
     $('#info').empty();
 });
+
+var myLocation = L.mapbox.featureLayer().addTo(map);
+
+map.on('locationfound', function(e) {
+
+    myLocation.setGeoJSON({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [e.latlng.lng, e.latlng.lat]
+        },
+        properties: {
+            'title': 'Here I am!',
+            'marker-color': '#ff8888',
+            'marker-symbol': 'star'
+        }
+    });
+
+});
